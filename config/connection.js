b22704299162a17cdd1e29fbc517b5500297c568
allen <allen.tuazon@gmail.com>
2023-08-08 20:29:06 +1000

    copying files to new repo for deployment

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,25 @@ require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
+
+sequelize = new Sequelize("railway", "root", "oWROjsDkMksFQDBtWdx9", {
+  host: "containers-us-west-37.railway.app",
+  dialect: "mysql",
+  port: 7198,
+});
 
 module.exports = sequelize;
